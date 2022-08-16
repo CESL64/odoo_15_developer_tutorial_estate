d40ce6b25521d8bdcd2536026fbab3d05b5d6168
CESL <carl.salinas64@gmail.com>
2022-08-16 08:41:20 -0600

    Using git with pycharm, updating file. Final Version of manifest.

--- __manifest__.py
+++ __manifest__.py
@@ -1,5 +1,16 @@
 {
     'name': "estate",
-    'depends': ['base'],
-    'application': True
+    'depends': ['base', 'contacts', 'mail'],
+    'application': True,
+    'data': [
+        'security/ir.model.access.csv',
+        #'security/security.xml',
+        'views/estate_property_views.xml',
+        'views/estate_property_type_views.xml',
+        'views/estate_property_tags_view.xml',
+        'views/estate_property_offer_view.xml',
+        'views/inherited_res_user.xml',
+        'views/estate_menus.xml',
+
+     ]
 }
